d0901df4243f956d63489b62fdc84bfa530b8291
Rob Nichols <git@nicholshayes.co.uk>
2013-04-30 09:22:28 +0100

    Initial bare rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pothole::Application.config.secret_token = '2bbc4ca6c8c27b96b2c59425675526c872cc01e2c6c9cf69281e5f812d21704b20c8e680967029e6d808c70295e26ad74aec07d44dac2381e66c79f95053fe9e'
